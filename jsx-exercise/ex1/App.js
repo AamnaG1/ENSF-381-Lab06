e2d056306e1b195dcddcef04821a130b02cfff86
AamnaG1 <aamna.ghimire1@ucalgary.ca>
2025-03-07 20:05:44 -0700

    Create App.js for Exercise1

---
+++ jsx-exercise/ex1/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import EngineeringTopics from './EngineeringTopics';
+
+function App() {
+  let currentYear = new Date();
+  var isLoggedIn = true;
+  let l
+  return (
+    <div> 
+      <h1> ENSF-381: Full Stack Web Development </h1>
+      <p> React Components.</p>
+      <p> {currentYear.getFullYear()} </p>
+      {
+        isLoggedIn == true ? <p> Welcome back! </p> : <p> Please Log in. </p>
+      }   
+  );
+}
+
+export default App;
