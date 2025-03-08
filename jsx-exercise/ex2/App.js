48f1e0e516e55055459bee499d51edbc6f0080ac
AamnaG1 <aamna.ghimire1@ucalgary.ca>
2025-03-07 23:09:04 -0700

    Update App.js

--- jsx-exercise/ex2/App.js
+++ jsx-exercise/ex2/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import About from './About';
 import Contact from './Contact';
 import Home from './Home';
-import EngineeringTopics from './EngineeringTopics';
 
 function App() {
   let currentYear = new Date();
